db7922b9195fd6a1cde170756872aa7109350168
Leonardo Bandeira <leobandeiraf@gmail.com>
2024-07-03 17:07:42 -0300

    Create UIView+Color.swift

---
+++ Lightning Study/Lightning Study/Design System/Tokens/Color/UIView+Color.swift
@@ -0,0 +1,54 @@
+import UIKit
+
+extension UIView {
+    func background(color: Color) {
+        self.backgroundColor = color.color
+    }
+    
+    @discardableResult
+    func border(edges: UIRectEdge, color: Color, inset: CGFloat = 0.0, thickness: CGFloat = 1.0) -> [UIView] {
+        var borders = [UIView]()
+        
+        @discardableResult
+        func addBorder(formats: String...) -> UIView {
+            let border = UIView(frame: .zero)
+            border.backgroundColor = color.color
+            border.translatesAutoresizingMaskIntoConstraints = false
+            addSubview(border)
+            addConstraints(formats.flatMap {
+                NSLayoutConstraint.constraints(
+                    withVisualFormat: $0,
+                    options: [],
+                    metrics: ["inset": inset, "thickness": thickness],
+                    views: ["border": border]
+                )
+            })
+            borders.append(border)
+            return border
+        }
+        
+        
+        if edges.contains(.top) || edges.contains(.all) {
+            addBorder(formats: "V:|-0-[border(==thickness)]", "H:|-inset-[border]-inset-|")
+        }
+        
+        if edges.contains(.bottom) || edges.contains(.all) {
+            addBorder(formats: "V:[border(==thickness)]-0-|", "H:|-inset-[border]-inset-|")
+        }
+        
+        if edges.contains(.left) || edges.contains(.all) {
+            addBorder(formats: "V:|-inset-[border]-inset-|", "H:|-0-[border(==thickness)]")
+        }
+        
+        if edges.contains(.right) || edges.contains(.all) {
+            addBorder(formats: "V:|-inset-[border]-inset-|", "H:[border(==thickness)]-0-|")
+        }
+        
+        return borders
+    }
+    
+    func border(size: CGFloat, color: Color) {
+        layer.borderColor = color.color.cgColor
+        layer.borderWidth = size
+    }
+}
