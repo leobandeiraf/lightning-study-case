122621479ed67b1ba48a7af127698c7d599615d8
Leonardo Bandeira <leobandeiraf@gmail.com>
2024-07-03 18:06:42 -0300

    Created variable componentes to ListCell

--- Lightning Study/Lightning Study/Scenes/List/Cell/ListCell.swift
+++ Lightning Study/Lightning Study/Scenes/List/Cell/ListCell.swift
@@ -2,6 +2,14 @@ import UIKit
 
 final class ListCell: TableViewCell {
     // MARK: - Component(s).
+    private lazy var publicKeyText = Text()
+    private lazy var aliasText = Text()
+    private lazy var channelsText = Text()
+    private lazy var capacityText = Text()
+    private lazy var firstSeenText = Text()
+    private lazy var updatedAtText = Text()
+    private lazy var cityText = Text()
+    private lazy var countryText = Text()
     
     // MARK: - Method(s).
     func configure(with model: String) -> Self {
