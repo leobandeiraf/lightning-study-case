b7c93ad6cad7b45e0d468bf9d1952104dfebcf90
Leonardo Bandeira <leobandeiraf@gmail.com>
2024-07-03 20:09:17 -0300

    Remove unnecessary code

--- Lightning Study/Lightning Study/Commons/Network/Builders/HeaderBuilder.swift
+++ Lightning Study/Lightning Study/Commons/Network/Builders/HeaderBuilder.swift
@@ -13,9 +13,6 @@ enum HeaderBuilder {
     private func basicHeader(with customHeaders: [String: String]? = nil) -> [String: String] {
         var headers: [String: String] = [:]
         headers["Content-Type"] = "application/json"
-        if let token = UserDefaults.standard.string(forKey: "Authorization") {
-            headers["Authorization"] = "Bearer \(token)"
-        }
         return headers
     }
 }
