d8ca1734dadbf0f000706dbc1009b15392ecf663
Leonardo Bandeira <leobandeiraf@gmail.com>
2024-07-03 17:06:59 -0300

    Create Reusable.swift

---
+++ Lightning Study/Lightning Study/Commons/Protocols/Reusable.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+public protocol Reusable: AnyObject {
+    static var identifier: String { get }
+}
+
+public extension Reusable {
+    static var identifier: String { return "\(self)"}
+}
