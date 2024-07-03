ad871c6b6604e16597760a2fc0cdcee256fa2882
Leonardo Bandeira <leobandeiraf@gmail.com>
2024-07-03 17:39:25 -0300

    Create ListCell.swift

---
+++ Lightning Study/Lightning Study/Scenes/List/Cell/ListCell.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+final class ListCell: TableViewCell {
+    // MARK: - Component(s).
+    
+    // MARK: - Method(s).
+    func configure(with model: String) -> Self {
+        return self
+    }
+    
+    // MARK: - View Configurable.
+    func buildViewHierarchy() { }
+    
+    func setupConstraints() { }
+}
