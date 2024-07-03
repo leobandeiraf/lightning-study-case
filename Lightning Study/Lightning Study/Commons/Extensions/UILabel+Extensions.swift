50a4762251da615d40664b09e03916fe9ee3c326
Leonardo Bandeira <leobandeiraf@gmail.com>
2024-07-03 18:06:27 -0300

    Move UILabel+Extensions to Extensions paste

--- Lightning Study/Lightning Study/Commons/Extensions/UILabel+Extensions.swift
+++ Lightning Study/Lightning Study/Commons/Extensions/UILabel+Extensions.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-public extension UITextField {
+extension UITextField {
     func font(_ fontStyle: any FontStyle) {
         if let font = UIFont(font: fontStyle.fontName, size: fontStyle.size) {
             let preferred = UIFontMetrics(forTextStyle: fontStyle.style)
@@ -9,7 +9,7 @@ public extension UITextField {
     }
 }
 
-public extension UINavigationBar {
+extension UINavigationBar {
     func largeTitleFont(_ fontStyle: any FontStyle) {
         if let font = UIFont(font: fontStyle.fontName, size: fontStyle.size) {
             let metrics = UIFontMetrics(forTextStyle: fontStyle.style)
