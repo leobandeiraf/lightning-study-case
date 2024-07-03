a73b867c82f45a09deb6f0ed3d3cb7ac32c639fd
Leonardo Bandeira <leobandeiraf@gmail.com>
2024-07-03 20:08:39 -0300

    Update NodesModel.swift

--- Lightning Study/Lightning Study/Scenes/List/Model/NodesModel.swift
+++ Lightning Study/Lightning Study/Scenes/List/Model/NodesModel.swift
@@ -1,28 +1,20 @@
 import Foundation
 
-struct NodesModel: Decodable {
-    let topByCapacity: [Node]
-    let topByChannels: [Node]
-    
-    struct Node: Decodable {
-        let publicKey: String
-        let alias: String
-        let capacity: Int?
-        let channels: Int?
-        let city: [Place]?
-        let country: [Place]?
-    }
-    
-    struct Place: Decodable {
-        let de, en, es, fr: String?
-        let ja, ptBR, ru: String?
-        let zhCN: String?
+struct Node: Decodable {
+    let publicKey, alias: String
+    let channels, capacity: Int
+    let firstSeen, updatedAt: Double
+    let city: Place?
+    let country: Place
+}
 
-        enum CodingKeys: String, CodingKey {
-            case de, en, es, fr, ja
-            case ptBR = "pt-BR"
-            case ru
-            case zhCN = "zh-CN"
-        }
+struct Place: Decodable {
+    let de, en, es, fr, ja, ptBR, ru, zhCN: String
+
+    enum CodingKeys: String, CodingKey {
+        case de, en, es, fr, ja
+        case ptBR = "pt-BR"
+        case ru
+        case zhCN = "zh-CN"
     }
 }
