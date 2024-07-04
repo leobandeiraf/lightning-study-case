139bf2327404084215faa0263ba0c56f49041526
Leonardo Bandeira <leobandeiraf@gmail.com>
2024-07-03 22:10:49 -0300

    Create ListViewModel+Tests.swift

---
+++ Lightning Study/Lightning StudyTests/List/ListViewModel+Tests.swift
@@ -0,0 +1,55 @@
+@testable import Lightning_Study
+import XCTest
+
+private class ListDisplayingSpy: ListDisplaying {
+    enum Message: Equatable {
+        case displayLoading(Bool)
+        case displayNodes([Node])
+    }
+
+    private(set) var messages: [Message] = []
+   
+    func displayLoading(_ value: Bool) {
+        messages.append(.displayLoading(true))
+    }
+    
+    func displayNodes(with nodes: [Node]) {
+        messages.append(.displayNodes(nodes))
+    }
+}
+
+private class ListServiceMock: ListServicing {
+    var nodeExpectedResult: Result<[Node], HTTPError> = .success([.fixture()])
+    func getTopNodes(completion: @escaping (Result<[Node], HTTPError>) -> Void) {
+        completion(nodeExpectedResult)
+    }
+}
+
+// MARK: - Setup SUT
+private extension ListViewModelTests {
+    typealias DependencySUT = (
+        sut: ListViewModel,
+        displaySpy: ListDisplayingSpy,
+        serviceMock: ListServiceMock
+    )
+
+    func makeSUT() -> DependencySUT {
+        let displaySpy = ListDisplayingSpy()
+        let serviceMock = ListServiceMock()
+
+        let sut = ListViewModel(service: serviceMock)
+        sut.viewController = displaySpy
+        return (sut, displaySpy, serviceMock)
+    }
+}
+
+// MARK: - Tests
+final class ListViewModelTests: XCTestCase {
+    func testGetNode_WhenSuccess_ShouldCallDisplayNodes() {
+        let args = makeSUT()
+
+        args.sut.getNodes()
+
+        XCTAssertEqual(args.displaySpy.messages, [.displayNodes([.fixture()])])
+    }
+}
