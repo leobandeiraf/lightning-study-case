217eee80fde9a70f14de902ed6040bc5ffa5c1b5
Leonardo Bandeira <leobandeiraf@gmail.com>
2024-07-04 11:26:45 -0300

    Remove loading call to coordinator

---
+++ Lightning Study/Lightning Study/Scenes/List/Views/ErrorView.swift
@@ -0,0 +1,18 @@
+import SnapKit
+import UIKit
+
+final class ErrorView: UIView {
+    // MARK: - Component(s).
+    
+}
+
+// MARK: - View Configurable.
+extension ErrorView: ViewConfigurable {
+    func buildViewHierarchy() {
+        <#code#>
+    }
+    
+    func setupConstraints() {
+        <#code#>
+    }
+}
