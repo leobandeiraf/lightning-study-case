854f1f1ba9cf8f72c51036b053f39c1a4c3e457e
Leonardo Bandeira <leobandeiraf@gmail.com>
2024-07-03 21:47:22 -0300

    Created model fixture

---
+++ Lightning Study/Lightning StudyTests/List/Model/NodesModel+Fixture.swift
@@ -0,0 +1,25 @@
+@testable import Lightning_Study
+
+extension Node {
+    static func fixture(
+        publicKey: String = String(),
+        alias: String = String(),
+        channels: Int = 0,
+        capacity: Int = 0,
+        firstSeen: Double = 0,
+        updatedAt: Double = 0,
+        city: Place? = nil,
+        country: Place? = nil
+    ) {
+        .init(
+            publicKey: publicKey,
+            alias: alias,
+            channels: channels,
+            capacity: capacity,
+            firstSeen: firstSeen,
+            updatedAt: updatedAt,
+            city: city?,
+            country: country?
+        )
+    }
+}
