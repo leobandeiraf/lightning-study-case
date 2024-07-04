90e654720e7891c927737a811a78d29643ab086a
Leonardo Bandeira <leobandeiraf@gmail.com>
2024-07-03 21:03:58 -0300

    Update HTTPProtocol.swift

--- Lightning Study/Lightning Study/Commons/Network/Protocols/HTTPProtocol.swift
+++ Lightning Study/Lightning Study/Commons/Network/Protocols/HTTPProtocol.swift
@@ -3,8 +3,8 @@ import Foundation
 protocol HTTPProtocol {
     func request<Model: Decodable>(
         service: NativeRequestType,
-        with model: Model.Type?,
-        completion: @escaping (Result<Model?, HTTPError>) -> Void
+        with model: Model.Type,
+        completion: @escaping (Result<Model, HTTPError>) -> Void
     )
     
     func cancel(completion: (() -> Void)?)
